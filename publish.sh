7de671a793c44c369e8da4413ee33e945483bc67
Massimo Zambelli <mass.zambelli@gmail.com>
2018-11-29 22:54:43 +0100

    email and name to CI vars

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ npm run production
 
 echo -e "Deploying to Github Pages..."
 
-git config user.email "mass.zambelli@gmail.com"
-git config user.name "Massimo Zambelli"
+git config user.email $GH_EMAIL
+git config user.name $GH_NAME
 gh-pages -d dist
 
