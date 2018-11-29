280e7b1d48f8d284a58b1e03dba447f756b6776c
Massimo Zambelli <mass.zambelli@gmail.com>
2018-11-29 22:37:45 +0100

    git config for ci

--- publish.sh
+++ publish.sh
@@ -3,5 +3,8 @@ echo -e "Building..."
 npm run production
 
 echo -e "Deploying to Github Pages..."
+
+git config user.email "mass.zambelli@gmail.com"
+git config user.name "Massimo Zambelli"
 gh-pages -d dist
 
